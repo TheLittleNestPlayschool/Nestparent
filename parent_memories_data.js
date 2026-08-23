1662d941f024903d93a0e5971951dd2c5416a0ed
TheLittleNestPlayschool <admin@thelittle-nest.com>
2026-08-23 16:25:07 +0800

    Create parent_memories_data.js

---
+++ parent_memories_data.js
@@ -0,0 +1,86 @@
+/*   memories overview*/
+
+export const memoriesOverview = {
+
+  title:
+    "Mia's Memories",
+
+  label:
+    "Her Little Story",
+
+  count:
+    143,
+
+  copy:
+    "From her very first days to the little moments that happened this morning.",
+
+  photo:
+    "https://images.unsplash.com/photo-1596464716127-f2a82984de30?auto=format&fit=crop&w=1200&q=86"
+
+};
+
+
+/*   memory chapters*/
+
+export const memoryChapters = [
+
+  {
+    id:
+      "today",
+
+    title:
+      "Today",
+
+    copy:
+      "12 new little moments",
+
+    count:
+      12
+  },
+
+
+  {
+    id:
+      "week",
+
+    title:
+      "This Week",
+
+    copy:
+      "A busy little week at The Little Nest",
+
+    count:
+      31
+  },
+
+
+  {
+    id:
+      "august",
+
+    title:
+      "August",
+
+    copy:
+      "Her August memories",
+
+    count:
+      64
+  },
+
+
+  {
+    id:
+      "first-days",
+
+    title:
+      "First Days",
+
+    copy:
+      "Where her Little Nest story began",
+
+    count:
+      18
+  }
+
+];
