931cea9f35d83539456f7189d9898a1e310e0634
TheLittleNestPlayschool <admin@thelittle-nest.com>
2026-08-23 15:49:10 +0800

    Create parent_nest_stage.js

---
+++ parent_nest_stage.js
@@ -0,0 +1,165 @@
+import {
+  createNestCard
+} from "./parent_nest_card.js";
+
+
+import {
+  renderCardPositions
+} from "./parent_card_positions.js";
+
+
+let nestOpen =
+  false;
+
+
+let nestCard =
+  null;
+
+
+/*   open nest stage*/
+
+export function openNestStage({
+  carousel,
+  activeIndex,
+  hideHint
+}){
+
+  if(
+    nestOpen
+    ||
+    !carousel
+  ){
+    return;
+  }
+
+
+  nestOpen =
+    true;
+
+
+  if(
+    typeof hideHint ===
+    "function"
+  ){
+
+    hideHint();
+
+  }
+
+
+  nestCard =
+    createNestCard();
+
+
+  carousel.appendChild(
+    nestCard
+  );
+
+
+  void nestCard.offsetWidth;
+
+
+  renderCardPositions({
+    carousel,
+    activeIndex,
+    nestOpen:true
+  });
+
+
+  requestAnimationFrame(
+    ()=>{
+
+      requestAnimationFrame(
+        ()=>{
+
+          if(
+            !nestCard
+          ){
+            return;
+          }
+
+
+          nestCard.classList.add(
+            "is-visible"
+          );
+
+        }
+      );
+
+    }
+  );
+
+}
+
+
+/*   close nest stage*/
+
+export function closeNestStage({
+  carousel,
+  activeIndex
+}){
+
+  if(
+    !nestOpen
+  ){
+    return;
+  }
+
+
+  nestOpen =
+    false;
+
+
+  if(
+    nestCard
+  ){
+
+    nestCard.classList.remove(
+      "is-visible"
+    );
+
+  }
+
+
+  renderCardPositions({
+    carousel,
+    activeIndex,
+    nestOpen:false
+  });
+
+
+  const cardToRemove =
+    nestCard;
+
+
+  nestCard =
+    null;
+
+
+  window.setTimeout(
+    ()=>{
+
+      if(
+        cardToRemove
+        &&
+        cardToRemove.parentNode
+      ){
+
+        cardToRemove.remove();
+
+      }
+
+    },
+    1250
+  );
+
+}
+
+
+/*   nest state*/
+
+export function isNestStageOpen(){
+
+  return nestOpen;
+
+}
