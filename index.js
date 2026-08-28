cb1d1c81096452e6e2440ac89a29b046f9f2aa26
TheLittleNestPlayschool <admin@thelittle-nest.com>
2026-08-28 12:09:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,53 +1,28 @@
-import {
-  applyTimeAtmosphere
-} from "./parent_time.js";
+import{applyTimeAtmosphere}from"./parent_time.js";
+import{buildCards,activateCarousel}from"./parent_carousel.js";
+import{activateSheet}from"./parent_sheet.js";
+import{activateNavigation}from"./parent_navigation.js";
+import{activateArrival}from"./parent_arrival.js";
+import{activateNestControl}from"./parent_nest_control.js";
+import{activateStageRouter}from"./parent_stage_router.js";
+import{activateParentAuth}from"./parent_auth.js";
 
-
-import {
-  buildCards,
-  activateCarousel
-} from "./parent_carousel.js";
-
-
-import {
-  activateSheet
-} from "./parent_sheet.js";
-
-
-import {
-  activateNavigation
-} from "./parent_navigation.js";
-
-
-import {
-  activateArrival
-} from "./parent_arrival.js";
-
-
-import {
-  activateNestControl
-} from "./parent_nest_control.js";
-
-
-import {
-  activateStageRouter
-} from "./parent_stage_router.js";
-
+let appStarted=false;
 
 /*   start parent app*/
+function startParentApp(){
+  if(appStarted) return;
+  appStarted=true;
 
-applyTimeAtmosphere();
+  applyTimeAtmosphere();
+  buildCards();
+  activateCarousel();
+  activateSheet();
+  activateNavigation();
+  activateArrival();
+  activateNestControl();
+  activateStageRouter();
+}
 
-buildCards();
-
-activateCarousel();
-
-activateSheet();
-
-activateNavigation();
-
-activateArrival();
-
-activateNestControl();
-
-activateStageRouter();
+/*   authenticate then start*/
+activateParentAuth(startParentApp);
