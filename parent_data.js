286f9f3f7a4a69f53530564de2e550703588b404
TheLittleNestPlayschool <admin@thelittle-nest.com>
2026-08-28 15:06:14 +0800

    Create parent_data.js

---
+++ parent_data.js
@@ -0,0 +1,56 @@
+const XANO_BASE_URL='https://x8ki-letl-twmt.n7.xano.io/api:wtEDiEuV';
+
+let parentData=null;
+
+/*   load parent data*/
+export async function loadParentData(){
+  const authToken=localStorage.getItem('authToken');
+
+  const response=await fetch(
+    `${XANO_BASE_URL}/pa_load_parent`,
+    {
+      method:'GET',
+      headers:{
+        'Authorization':`Bearer ${authToken}`
+      }
+    }
+  );
+
+  const data=await response.json();
+
+  if(!response.ok){
+    throw new Error(
+      data.message||
+      'Unable to load parent data.'
+    );
+  }
+
+  parentData=data;
+
+  return data;
+}
+
+/*   get parent data*/
+export function getParentData(){
+  return parentData;
+}
+
+/*   get parent*/
+export function getParent(){
+  return parentData?.parent||null;
+}
+
+/*   get student*/
+export function getStudent(){
+  return parentData?.student||null;
+}
+
+/*   get franchise*/
+export function getFranchise(){
+  return parentData?.franchise||null;
+}
+
+/*   get student media*/
+export function getStudentMedia(){
+  return parentData?.student_medias||[];
+}
