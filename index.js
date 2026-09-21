fa86449e1d2d300ada483316030cf7c9a0234483
TheLittleNestPlayschool <admin@thelittle-nest.com>
2026-09-21 20:12:45 +0800

    Refresh Our Nest privacy modules

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ import{buildCards,activateCarousel}from"./parent_carousel.js";
 import{activateSheet}from"./parent_sheet.js";
 import{activateNavigation}from"./parent_navigation.js";
 import{activateArrival}from"./parent_arrival.js";
-import{activateNestControl}from"./parent_nest_control.js";
-import{activateStageRouter}from"./parent_stage_router.js";
+import{activateNestControl}from"./parent_nest_control.js?v=2";
+import{activateStageRouter}from"./parent_stage_router.js?v=2";
 import{activateParentAuth}from"./parent_auth.js?v=2";
 import{
   startParentAnalyticsSession,
