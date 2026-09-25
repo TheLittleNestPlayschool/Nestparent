a8dd3923ffe0f77cadc3268f642d27261b100c30
TheLittleNestPlayschool <admin@thelittle-nest.com>
2026-09-25 22:11:55 +0800

    Refresh NestHome install-flow cache

--- test/sw.js
+++ test/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME='nesthome-test-v2';
-const APP_SHELL=['./','./index.html','./test.css?v=3','./test.js?v=2'];
+const CACHE_NAME='nesthome-test-v3';
+const APP_SHELL=['./','./index.html','./test.css?v=4','./test.js?v=3'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))));self.clients.claim();});
-self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request,{cache:'no-store'}).catch(()=>caches.match(event.request)));});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request,{cache:'no-store'}).then(response=>{const copy=response.clone();caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy));return response;}).catch(()=>caches.match(event.request)));});
