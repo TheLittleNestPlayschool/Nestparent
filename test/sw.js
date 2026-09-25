9724f8338daeffda70330426d87f9a3182368f81
TheLittleNestPlayschool <admin@thelittle-nest.com>
2026-09-25 22:03:52 +0800

    Refresh NestHome test PWA cache

--- test/sw.js
+++ test/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME='nesthome-test-v1';
-const APP_SHELL=['./','./index.html','./test.css','./test.js'];
+const CACHE_NAME='nesthome-test-v2';
+const APP_SHELL=['./','./index.html','./test.css?v=3','./test.js?v=2'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))));self.clients.claim();});
-self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).catch(()=>caches.match(event.request)));});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request,{cache:'no-store'}).catch(()=>caches.match(event.request)));});
