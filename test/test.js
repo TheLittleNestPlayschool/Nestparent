ee0da485e9fd3b41da45a83931163e0d18f30b3d
TheLittleNestPlayschool <admin@thelittle-nest.com>
2026-09-25 20:59:10 +0800

    Refresh test/test.js for NestHome test deployment

--- test/test.js
+++ test/test.js
@@ -5,3 +5,4 @@ import{activateParentAuth}from"../parent_auth.js?v=2";
 let appStarted=false;
 async function startParentApp(){if(appStarted)return;appStarted=true;try{await loadParentData();applyTimeAtmosphere();applyParentGreeting();}catch(error){appStarted=false;console.error("Unable to start NestHome test:",error);}}
 activateParentAuth(startParentApp);
+ 
