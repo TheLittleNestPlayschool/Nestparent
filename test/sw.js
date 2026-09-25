fc9103f9f5aede3e3d7f6582ce80062d76d4b533
TheLittleNestPlayschool <admin@thelittle-nest.com>
2026-09-25 22:27:10 +0800

    Refresh NestHome install guidance cache

--- test/sw.js
+++ test/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME='nesthome-test-v3';
-const APP_SHELL=['./','./index.html','./test.css?v=4','./test.js?v=3'];
+const CACHE_NAME='nesthome-test-v4';
+const APP_SHELL=['./','./index.html','./test.css?v=4','./test.js?v=4'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))));self.clients.claim();});
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request,{cache:'no-store'}).then(response=>{const copy=response.clone();caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy));return response;}).catch(()=>caches.match(event.request)));});
